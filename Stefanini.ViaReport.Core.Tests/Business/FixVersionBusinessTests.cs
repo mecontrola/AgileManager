6b081f4e456caba4a6e929e42546b6c8ba453df4
Rafael Almeida De Oliveira <raoliveira9@stefanini.com>
2022-03-18 14:56:24 -0300

    AHM-9 Adicionado teste de FixVersionBusiness e ajuste no grid da tela de itens sem versão.

---
+++ Stefanini.ViaReport.Core.Tests/Business/FixVersionBusinessTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Stefanini.ViaReport.Core.Business;
+using Stefanini.ViaReport.Core.Mappers;
+using Stefanini.ViaReport.Core.Tests.Mocks;
+using Stefanini.ViaReport.Core.Tests.Mocks.Dto;
+using Stefanini.ViaReport.Core.Tests.Mocks.Services;
+using Stefanini.ViaReport.Core.Tests.TestUtils;
+using Xunit;
+
+namespace Stefanini.ViaReport.Core.Tests.Business
+{
+    public class FixVersionBusinessTests : BaseAsyncMethods
+    {
+        private readonly IFixVersionBusiness business;
+
+        public FixVersionBusinessTests()
+        {
+            var issuesNotFixVersionService = IssuesNotFixVersionServiceMock.Create();
+            var issueGet = IssueGetMock.Create();
+            var issueDtoToIssueInfoDtoMapper = new IssueDtoToIssueInfoDtoMapper();
+
+            business = new FixVersionBusiness(issuesNotFixVersionService, issueDtoToIssueInfoDtoMapper, issueGet);
+        }
+
+        [Fact(DisplayName = "[FixVersionBusiness.GetListIssuesNoFixVersion]")]
+        public async void Deve()
+        {
+            var expected = IssueInfoDtoMock.CreateDoneList();
+            var actual = await business.GetListIssuesNoFixVersion(DataMock.VALUE_USERNAME,
+                                                                  DataMock.VALUE_PASSWORD,
+                                                                  DataMock.TEXT_SEARCH_PROJECT,
+                                                                  GetCancellationToken());
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
