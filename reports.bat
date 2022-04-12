1ad79ae8fccadf79e81f518073a48ad83632d6ec
Rafael Almeida De Oliveira <raoliveira9@stefanini.com>
2022-04-12 17:50:04 -0300

    AHM-15 Ajustado o script para gerar o relatório de cobertura para combinar os relatórios de todos os projetos em um único relatório.

--- reports.bat
+++ reports.bat
@@ -9,19 +9,19 @@ dotnet restore --force %~dp0Stefanini.sln
 dotnet build %~dp0Stefanini.sln
 
 dotnet test %~dp0Stefanini.sln ^
-            --logger "trx;LogFileName=TestResults.trx" ^
-            --logger "xunit;LogFileName=TestResults.xml" ^
             --results-directory %~dp0BuildReports/UnitTests ^
             --collect:"XPlat Code Coverage" ^
+            --no-restore ^
             --no-build ^
+            --verbosity=minimal ^
             /p:CollectCoverage=true ^
             /p:CoverletOutput="%~dp0BuildReports/Coverage/" ^
             /p:CoverletOutputFormat=cobertura ^
             /p:Exclude="[xunit.*]*
 
 dotnet "%~dp0tools\ReportGenerator.dll" ^
-           -reports:%~dp0BuildReports\Coverage\coverage.cobertura.xml ^
+           -reports:%~dp0BuildReports\UnitTests\**\*.cobertura.xml ^
            -targetdir:%~dp0BuildReports\Coverage ^
            -reporttypes:HTML;HTMLSummary
-
+pause
 start "report" "%~dp0BuildReports\Coverage\index.html"
