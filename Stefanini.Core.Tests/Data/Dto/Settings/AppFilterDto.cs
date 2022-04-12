debe2e4335d54253cc5255596955104594f9749c
Rafael Almeida De Oliveira <raoliveira9@stefanini.com>
2022-04-12 17:48:19 -0300

    AHM-15 Separado os testes de Stefanini.ViaReport.Core.Tests e criado projeto de testes Stefanini.Core.Tests.

---
+++ Stefanini.Core.Tests/Data/Dto/Settings/AppFilterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stefanini.Core.Data.Dto.Settings
+{
+    public class AppFilterDto
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Quarter { get; set; }
+    }
+}
---
+++ Stefanini.Core.Tests/Data/Dto/Settings/AppSettingsDto.cs
@@ -0,0 +1,10 @@
+﻿using Stefanini.Core.Settings;
+
+namespace Stefanini.Core.Data.Dto.Settings
+{
+    public class AppSettingsDto : UserSettings
+    {
+        public bool PersistFilter { get; set; }
+        public AppFilterDto FilterData { get; set; }
+    }
+}
